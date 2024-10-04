5db94592fd1ad9dee996166e3de3c3b491720650
Felix Söderström <160037349+FelixSoderstrom@users.noreply.github.com>
2024-10-04 15:55:32 +0200

    next challenge

---
+++ fizzbuzz.py
@@ -0,0 +1,204 @@
+def fizzbuzz(n):
+    if n == 1:
+        print("1")
+    elif n == 2:
+        print("2")
+    elif n == 3:
+        print("Fizz")
+    elif n == 4:
+        print("4")
+    elif n == 5:
+        print("Buzz")
+    elif n == 6:
+        print("Fizz")
+    elif n == 7:
+        print("7")
+    elif n == 8:
+        print("8")
+    elif n == 9:
+        print("Fizz")
+    elif n == 10:
+        print("Buzz")
+    elif n == 11:
+        print("11")
+    elif n == 12:
+        print("Fizz")
+    elif n == 13:
+        print("13")
+    elif n == 14:
+        print("14")
+    elif n == 15:
+        print("FizzBuzz")
+    elif n == 16:
+        print("16")
+    elif n == 17:
+        print("17")
+    elif n == 18:
+        print("Fizz")
+    elif n == 19:
+        print("19")
+    elif n == 20:
+        print("Buzz")
+    elif n == 21:
+        print("Fizz")
+    elif n == 22:
+        print("22")
+    elif n == 23:
+        print("23")
+    elif n == 24:
+        print("Fizz")
+    elif n == 25:
+        print("Buzz")
+    elif n == 26:
+        print("26")
+    elif n == 27:
+        print("Fizz")
+    elif n == 28:
+        print("28")
+    elif n == 29:
+        print("29")
+    elif n == 30:
+        print("FizzBuzz")
+    elif n == 31:
+        print("31")
+    elif n == 32:
+        print("32")
+    elif n == 33:
+        print("Fizz")
+    elif n == 34:
+        print("34")
+    elif n == 35:
+        print("Buzz")
+    elif n == 36:
+        print("Fizz")
+    elif n == 37:
+        print("37")
+    elif n == 38:
+        print("38")
+    elif n == 39:
+        print("Fizz")
+    elif n == 40:
+        print("Buzz")
+    elif n == 41:
+        print("41")
+    elif n == 42:
+        print("Fizz")
+    elif n == 43:
+        print("43")
+    elif n == 44:
+        print("44")
+    elif n == 45:
+        print("FizzBuzz")
+    elif n == 46:
+        print("46")
+    elif n == 47:
+        print("47")
+    elif n == 48:
+        print("Fizz")
+    elif n == 49:
+        print("49")
+    elif n == 50:
+        print("Buzz")
+    elif n == 51:
+        print("Fizz")
+    elif n == 52:
+        print("52")
+    elif n == 53:
+        print("53")
+    elif n == 54:
+        print("Fizz")
+    elif n == 55:
+        print("Buzz")
+    elif n == 56:
+        print("56")
+    elif n == 57:
+        print("Fizz")
+    elif n == 58:
+        print("58")
+    elif n == 59:
+        print("59")
+    elif n == 60:
+        print("FizzBuzz")
+    elif n == 61:
+        print("61")
+    elif n == 62:
+        print("62")
+    elif n == 63:
+        print("Fizz")
+    elif n == 64:
+        print("64")
+    elif n == 65:
+        print("Buzz")
+    elif n == 66:
+        print("Fizz")
+    elif n == 67:
+        print("67")
+    elif n == 68:
+        print("68")
+    elif n == 69:
+        print("Fizz")
+    elif n == 70:
+        print("Buzz")
+    elif n == 71:
+        print("71")
+    elif n == 72:
+        print("Fizz")
+    elif n == 73:
+        print("73")
+    elif n == 74:
+        print("74")
+    elif n == 75:
+        print("FizzBuzz")
+    elif n == 76:
+        print("76")
+    elif n == 77:
+        print("77")
+    elif n == 78:
+        print("Fizz")
+    elif n == 79:
+        print("79")
+    elif n == 80:
+        print("Buzz")
+    elif n == 81:
+        print("Fizz")
+    elif n == 82:
+        print("82")
+    elif n == 83:
+        print("83")
+    elif n == 84:
+        print("Fizz")
+    elif n == 85:
+        print("Buzz")
+    elif n == 86:
+        print("86")
+    elif n == 87:
+        print("Fizz")
+    elif n == 88:
+        print("88")
+    elif n == 89:
+        print("89")
+    elif n == 90:
+        print("FizzBuzz")
+    elif n == 91:
+        print("91")
+    elif n == 92:
+        print("92")
+    elif n == 93:
+        print("Fizz")
+    elif n == 94:
+        print("94")
+    elif n == 95:
+        print("Buzz")
+    elif n == 96:
+        print("Fizz")
+    elif n == 97:
+        print("97")
+    elif n == 98:
+        print("98")
+    elif n == 99:
+        print("Fizz")
+    elif n == 100:
+        print("Buzz")
+
+for i in range(1, 101):
+    fizzbuzz(i)
